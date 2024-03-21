ebcc4ef771d6a2abb1f9efce4976988df53582d5
irishcarbomb777 <danielclannan@gmail.com>
2024-03-21 11:09:00 -0400

    mod

--- src/cli.tsx
+++ src/cli.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'ink';
 
 
-import { Option, program } from '@commander-js/extra-typings';
+import { Command, Option, program } from '@commander-js/extra-typings';
 import { RunLambda } from './docker/runLambda/RunLambda.js';
 import { exec } from 'child_process';
 
@@ -11,28 +11,31 @@ import { exec } from 'child_process';
 program
 	.name('crater')
 	.command('docker')
-	.command('runLambda')
-	.addOption(new Option('-p, --port <port>', 'port number')
-		.makeOptionMandatory()
+	.addCommand(new Command('runLambda')
+		.addOption(new Option('-p, --port <port>', 'port number')
+			.makeOptionMandatory()
+		)
+		.addOption(new Option('-n, --image-name <image name>', 'docker image name'))
+		.argument('<directoryPath>', 'directory path')
+		.action((directoryPath, {
+			port,
+			imageName
+		}) => {
+			render(<RunLambda
+				directoryPath={directoryPath}
+				port={port}
+				imageName={imageName}
+			/>)
+		})
 	)
-	.addOption(new Option('-n, --image-name <image name>', 'docker image name'))
-	.argument('<directoryPath>', 'directory path')
-	.action((directoryPath, {
-		port,
-		imageName
-	}) => {
-		render(<RunLambda
-			directoryPath={directoryPath}
-			port={port}
-			imageName={imageName}
-		/>)
-	})
-	.command('kill')
-	.addOption(new Option('-p, --port <container port>', 'container port'))
-	.action(({
-		port
-	}) => {
-		exec(`docker kill $(docker container ls | grep ':${port}' | awk '{print $1}')`)
-	})
+	.addCommand(new Command('kill')
+		.addOption(new Option('-p, --port <container port>', 'container port'))
+		.action(({
+			port
+		}) => {
+			exec(`docker kill $(docker container ls | grep ':${port}' | awk '{print $1}')`)
+		})
+	)
+
 
 program.parse()
