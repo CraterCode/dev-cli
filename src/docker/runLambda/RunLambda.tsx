695b49bb715d75ef70d624de1414ddcc9aecbfc3
irishcarbomb777 <danielclannan@gmail.com>
2024-03-21 12:20:57 -0400

    mod

--- src/docker/runLambda/RunLambda.tsx
+++ src/docker/runLambda/RunLambda.tsx
@@ -3,7 +3,7 @@ import { Text } from "ink";
 import { FC, useEffect, useState } from "react";
 import React from 'react';
 import Spinner from 'ink-spinner';
-
+import Docker from 'dockerode';
 export const RunLambda: FC<{
     directoryPath: string;
     port: string
@@ -17,71 +17,87 @@ export const RunLambda: FC<{
         const [running, setRunning] = useState(false);
         const [imageId, setImageId] = useState<string | null>(null);
         useEffect(() => {
-            exec(`docker build -q -t ${imageName ?? 'lambda-image'} ${directoryPath}`, (error, stdout, stderr) => {
+            const docker = new Docker()
+            docker.buildImage({ context: directoryPath, src: ['Dockerfile'] }, { t: imageName ?? 'lambda-image' }, (error, stream) => {
                 if (error) {
                     console.error(`error: ${error.message}`);
                     return;
                 }
-                if (stderr) {
-                    console.error(`stderr: ${stderr}`);
-                    return;
+                if (stream) {
+                    stream.on('data', (data) => {
+                        console.log(data.toString());
+                    });
+                    stream.on('end', () => {
+                        console.log('Image built');
+                        setBuilding(false);
+                    });
                 }
-                console.log(`stdout: ${stdout}`);
-                setImageId(stdout.replaceAll('sha256:', ''));
-                setBuilding(false);
             })
+            // exec(`docker build -q -t ${imageName ?? 'lambda-image'} ${directoryPath}`, (error, stdout, stderr) => {
+            //     if (error) {
+            //         console.error(`error: ${error.message}`);
+            //         return;
+            //     }
+            //     if (stderr) {
+            //         console.error(`stderr: ${stderr}`);
+            //         return;
+            //     }
+            //     console.log(`stdout: ${stdout}`);
+            //     setImageId(stdout.replaceAll('sha256:', ''));
+            //     setBuilding(false);
+            // })
         }, [])
-        useEffect(() => {
-            if (imageId) {
-                // Define your command and arguments
-                const command = 'docker';
-                const args = [
-                    'run',
-                    '--platform', '"linux/amd64"',
-                    '-p', `${port}:8080`,
-                    '-d', `"${imageId}"`
-                ];
-                const dockerProcess = spawn('docker', [
-                    'run',
-                    '--platform linux/amd64',
-                    `-p ${port}:8080`,
-                    `-d ${imageId}`
-                ], { shell: true })
-                const commandString = command + ' ' + args.join(' ');
-                console.log('Executing command:', commandString);
-                console.log('Docker process started')
-                dockerProcess.stdout.on('data', (data) => {
-                    console.log(`stdout: ${data}`);
-                    // Set running to true or do something else
-                });
+        // useEffect(() => {
+        //     if (imageId) {
+        //         // Define your command and arguments
+        //         const command = 'docker';
+        //         const args = [
+        //             'run',
+        //             '--platform', '"linux/amd64"',
+        //             '-p', `${port}:8080`,
+        //             '-d', `"${imageId}"`
+        //         ];
+        //         const dockerProcess = spawn('docker', [
+        //             'run',
+        //             '--platform linux/amd64',
+        //             `-p ${port}:8080`,
+        //             `-d ${imageId}`
+        //         ], { shell: true })
+        //         const commandString = command + ' ' + args.join(' ');
+        //         console.log('Executing command:', commandString);
+        //         console.log('Docker process started')
+        //         dockerProcess.stdout.on('data', (data) => {
+        //             console.log(`stdout: ${data}`);
+        //             // Set running to true or do something else
+        //         });
 
-                dockerProcess.stderr.on('data', (data) => {
-                    console.error(`stderr: ${data}`);
-                });
+        //         dockerProcess.stderr.on('data', (data) => {
+        //             console.error(`stderr: ${data}`);
+        //         });
 
-                dockerProcess.on('exit', (code, signal) => {
-                    console.log(`Docker process exited with code ${code} and signal ${signal}`);
-                    // Set running to false or do some cleanup
-                });
+        //         dockerProcess.on('exit', (code, signal) => {
+        //             console.log(`Docker process exited with code ${code} and signal ${signal}`);
+        //             // Set running to false or do some cleanup
+        //         });
 
-                dockerProcess.on('error', (err) => {
-                    console.error('Failed to start Docker process:', err);
-                });
-                process.stdin.resume()
-                // exec(`docker run --platform linux/amd64 -p ${port}:8080 -d ${imageId}`, (error, stdout, stderr) => {
-                //     if (error) {
-                //         console.error(`error: ${error.message}`);
-                //         return;
-                //     }
-                //     if (stderr) {
-                //         console.error(`stderr: ${stderr}`);
-                //         return;
-                //     }
-                //     console.log(`stdout: ${stdout}`);
-                //     setRunning(true);
-                // })
-            }
-        }, [imageId])
+        //         dockerProcess.on('error', (err) => {
+        //             console.error('Failed to start Docker process:', err);
+        //         });
+        //         process.stdin.resume()
+        //         // exec(`docker run --platform linux/amd64 -p ${port}:8080 -d ${imageId}`, (error, stdout, stderr) => {
+        //         //     if (error) {
+        //         //         console.error(`error: ${error.message}`);
+        //         //         return;
+        //         //     }
+        //         //     if (stderr) {
+        //         //         console.error(`stderr: ${stderr}`);
+        //         //         return;
+        //         //     }
+        //         //     console.log(`stdout: ${stdout}`);
+        //         //     setRunning(true);
+        //         // })
+        //     }
+        // }, [imageId])
         return (<>
             {building && <Text>
                 <Text color='green'><Spinner type='dots' /> </Text>
